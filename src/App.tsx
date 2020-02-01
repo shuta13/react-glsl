3145dd65935c1ea0a65727031de0040e7e7c225d
shuta13 <hollystarsun@gmail.com>
2020-02-01 17:14:37 +0900

    wip: added gl-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import Canvas from '../src/components/Canvas'
+import { Surface } from 'gl-react-dom';
+import Canvas from '../src/components/Canvas';
 
 const App: React.FC = () => {
   return (
-    <Canvas></Canvas>
+    <Surface width={300} height={300}>
+      <Canvas></Canvas>
+    </Surface>
   );
 }
 
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Shaders, Node, GLSL } from 'gl-react';
 
 import './Canvas.scss'
 
+const shaders = Shaders.create({
+  helloGL: {
+    frag: GLSL`
+      precision highp float;
+      varying vec2 uv;
+      uniform float blue;
+      void main() {
+        gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+      }
+    `
+  }
+});
+
 const Canvas: React.FC = () => (
-  <div className="CanvasWrap">
-    This is Canvas
-  </div>
+  <Node shader={shaders.helloGL}></Node>
 );
 
 export default Canvas;
