9a7eb02815fdbd250593b1b6ab9832c6f3f446ad
shuta13 <hollystarsun@gmail.com>
2020-02-02 01:17:55 +0900

    fix: change dir

--- src/components/partials/Canvas.tsx
+++ src/components/partials/Canvas.tsx
@@ -1,46 +1,14 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { Surface } from 'gl-react-dom';
-import { Shaders, Node, GLSL } from 'gl-react';
+import React from 'react';
+import SampleShader from './SampleShader';
+
 import './Canvas.scss';
-import useGetWindowSize from '../hooks/useGetWindowSize'
-
-const fragment = require('./shaders/fragment.glsl');
-
-const shaders = Shaders.create({
-  smoke: {
-    frag: GLSL`${fragment.default}`
-  }
-});
-
-let payload = 0;
 
 const Canvas: React.FC = () => {
-  const { width, height } = useGetWindowSize();
-
-  // timer for animate
-  const [timer, setTimer] = useState(0);
-  const animate = useCallback(() => {
-    payload += 0.018;
-    setTimer(payload);
-    window.requestAnimationFrame(animate);
-  }, []);
-  useEffect(() => {
-    animate();
-  }, [animate]);
-
+  // add flag for menu here
   return (
     <div className="CanvasWrap">
       <div className="CanvasClip">
-        <Surface width={400} height={400}>
-          <Node
-            shader={shaders.smoke}
-            uniforms={{
-              shift: 1.6,
-              time: timer,
-              speed: [1.0, 1.0],
-              resolution: [width, height]
-            }} />
-        </Surface>
+        <SampleShader />
       </div>
     </div>
   );
---
+++ src/components/partials/SampleShader.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { Surface } from 'gl-react-dom';
+import { Shaders, Node, GLSL } from 'gl-react';
+import useGetWindowSize from '../hooks/useGetWindowSize'
+
+const fragment = require('./shaders/fragment.glsl');
+
+const shaders = Shaders.create({
+  smoke: {
+    frag: GLSL`${fragment.default}`
+  }
+});
+
+let payload = 0;
+
+const SampleShader: React.FC = () => {
+  const { width, height } = useGetWindowSize();
+
+  // timer for animate
+  const [timer, setTimer] = useState(0);
+  const animate = useCallback(() => {
+    payload += 0.018;
+    setTimer(payload);
+    window.requestAnimationFrame(animate);
+  }, []);
+  useEffect(() => {
+    animate();
+  }, [animate]);
+
+  return (
+    <Surface width={400} height={400}>
+      <Node
+        shader={shaders.smoke}
+        uniforms={{
+          shift: 1.6,
+          time: timer,
+          speed: [1.0, 1.0],
+          resolution: [width, height]
+        }} />
+    </Surface>
+  );
+};
+
+export default SampleShader;
