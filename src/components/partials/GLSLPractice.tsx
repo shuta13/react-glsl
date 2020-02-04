6704cffc86ca78bd89518f1f9b417093f9471326
shuta13 <hollystarsun@gmail.com>
2020-02-05 02:10:43 +0900

    wip

--- src/components/partials/GLSLPractice.tsx
+++ src/components/partials/GLSLPractice.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { Surface } from 'gl-react-dom';
 import { Shaders, Node, GLSL } from 'gl-react';
+import useGetWindowSize from '../hooks/useGetWindowSize';
 
 const fragment = require('./shaders/GLSLPractice/frag.glsl');
 
@@ -10,11 +11,31 @@ const shaders = Shaders.create({
   }
 });
 
+let payload = 0;
+
 const GLSLPractice: React.FC = () => {
+  const { width, height } = useGetWindowSize();
+  const requestRef = useRef(0);
+
+  // timer for animate
+  const [timer, setTimer] = useState(0);
+  const animate = useCallback(() => {
+    payload += 0.018;
+    setTimer(payload);
+    requestRef.current = window.requestAnimationFrame(animate);
+  }, []);
+  useEffect(() => {
+    requestRef.current = window.requestAnimationFrame(animate);
+    return () => window.cancelAnimationFrame(requestRef.current);
+  }, [animate]);
   return (
     <Surface width={400} height={400}>
       <Node
         shader={shaders.practice}
+        uniforms={{
+          // t: timer,
+          r: [width, height]
+        }}
       />
     </Surface>
   );
--- src/components/partials/shaders/GLSLPractice/frag.glsl
+++ src/components/partials/shaders/GLSLPractice/frag.glsl
@@ -3,5 +3,7 @@ uniform float t; // time
 uniform vec2  r; // resolution
 
 void main(void){
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y); // 正規化
+    float l = 0.1 / length(p);
+    gl_FragColor = vec4(vec3(l), 1.0);
 }
