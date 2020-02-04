7783445765084dcb757298f15939187e250f2aec
shuta13 <hollystarsun@gmail.com>
2020-02-05 01:30:56 +0900

    feat: setup practice

--- src/components/partials/Canvas.tsx
+++ src/components/partials/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import SampleShader from './SampleShader';
 import GLSLPractice from './GLSLPractice';
@@ -6,16 +6,25 @@ import GLSLPractice from './GLSLPractice';
 import './Canvas.scss';
 
 const Canvas: React.FC = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState('SampleShader');
   const changeCanvas = (payload: string) => {
     setName(payload);
+  }
+  const RenderCanvas = () => {
+    switch (name) {
+      case 'SampleShader':
+        return <SampleShader />;
+      case 'GLSLPractice':
+        return <GLSLPractice />;
+      default:
+        return <div>plz select</div>;
+    }
   }
   return (
     <div className="CanvasWrap">
       <div className="CanvasClipWrap">
         <div className="CanvasClip">
-          { name === 'SampleShader' && <SampleShader /> }
-          { name === 'GLSLPractice' && <GLSLPractice /> }
+          <RenderCanvas />
         </div>
         <Menu changeCanvas={changeCanvas} />
       </div>
