14b9d3689e40dff02d7a2e1d2b1726389d8b6c6f
shuta13 <hollystarsun@gmail.com>
2020-02-05 01:36:06 +0900

    fix: fix

--- src/components/partials/GLSLPractice.tsx
+++ src/components/partials/GLSLPractice.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Surface } from 'gl-react-dom';
 import { Shaders, Node, GLSL } from 'gl-react';
-// import useGetWindowSize from '../hooks/useGetWindowSize';
 
 const fragment = require('./shaders/GLSLPractice/frag.glsl');
 
@@ -11,22 +10,7 @@ const shaders = Shaders.create({
   }
 });
 
-// let payload = 0;
-
 const GLSLPractice: React.FC = () => {
-  // const { width, height } = useGetWindowSize();
-
-  // timer for animate
-  // const [timer, setTimer] = useState(0);
-  // const animate = useCallback(() => {
-  //   payload += 0.018;
-  //   setTimer(payload);
-  //   window.requestAnimationFrame(animate);
-  // }, []);
-  // useEffect(() => {
-  //   animate();
-  // }, [animate]);
-
   return (
     <Surface width={400} height={400}>
       <Node
