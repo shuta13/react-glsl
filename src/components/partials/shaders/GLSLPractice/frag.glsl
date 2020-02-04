bfa0bfad07cbdd3167f9896f1ede8092072810ec
shuta13 <hollystarsun@gmail.com>
2020-02-05 02:25:39 +0900

    wip

--- src/components/partials/shaders/GLSLPractice/frag.glsl
+++ src/components/partials/shaders/GLSLPractice/frag.glsl
@@ -3,7 +3,8 @@ uniform float t; // time
 uniform vec2  r; // resolution
 
 void main(void){
-    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y); // 正規化
-    float l = 0.1 * abs(sin(t)) / length(p);
+    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
+    p += vec2(cos(t * 5.0), sin(t)) * 0.5;
+    float l = 0.1 / length(p);
     gl_FragColor = vec4(vec3(l), 1.0);
 }
